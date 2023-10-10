1320ab9b1a62fc790075911c974da024a6bbca58
BlueGamesNetworkPMMP <140811591+Terpzz@users.noreply.github.com>
2023-10-10 03:42:26 -0700

    Update Main.php

--- src/ChestKits/Main.php
+++ src/ChestKits/Main.php
@@ -4,23 +4,16 @@ declare(strict_types=1);
 
 namespace ChestKits;
 
-use pocketmine\block\Block;
-
 use pocketmine\command\{Command, CommandSender};
-
 use pocketmine\inventory\ChestInventory;
-
-use pocketmine\item\{Item, ItemFactory};
-
+use pocketmine\item\Item;
 use pocketmine\{Player, Server};
-
 use pocketmine\plugin\PluginBase;
-
-use pocketmine\nbt\{NBT, tag\CompoundTag, tag\IntTag, tag\ListTag, tag\StringTag};
-
-use pocketmine\tile\{Tile, Chest};
-
+use pocketmine\nbt\{NBT, tag\CompoundTag, tag\ListTag, tag\StringTag};
+use pocketmine\tile\Chest;
 use pocketmine\utils\TextFormat as C;
+use pocketmine\block\VanillaBlocks;
+use pocketmine\item\VanillaItems;
 
 class Main extends PluginBase {
 
@@ -36,18 +29,18 @@ class Main extends PluginBase {
         }
         switch($command){
             case "kit":
-                $helmet = Item::get(306, 0, 1);
+                $helmet = VanillaItems::DIAMOND_HELMET(); // Use VanillaItems::DIAMOND_HELMET() to create a diamond helmet
                 $helmet->setCustomName("Kit Helmet");
-                $chestplate = Item::get(307, 0, 1);
+                $chestplate = VanillaItems::DIAMOND_CHESTPLATE(); // Use VanillaItems::DIAMOND_CHESTPLATE() to create a diamond chestplate
                 $chestplate->setCustomName("Kit Chestplate");
-                $leggings = Item::get(308, 0, 1);
+                $leggings = VanillaItems::DIAMOND_LEGGINGS(); // Use VanillaItems::DIAMOND_LEGGINGS() to create diamond leggings
                 $leggings->setCustomName("Kit Leggings");
-                $boots = Item::get(309, 0, 1);
+                $boots = VanillaItems::DIAMOND_BOOTS(); // Use VanillaItems::DIAMOND_BOOTS() to create diamond boots
                 $boots->setCustomName("Kit Boots");
-                $sword = Item::get(267, 0, 1);
+                $sword = VanillaItems::DIAMOND_SWORD(); // Use VanillaItems::DIAMOND_SWORD() to create a diamond sword
                 $sword->setCustomName("Kit Sword");
                 $nbt = new CompoundTag("BlockEntityTag", [new ListTag("Items", [$helmet->nbtSerialize(0), $chestplate->nbtSerialize(1), $leggings->nbtSerialize(2), $boots->nbtSerialize(3), $sword->nbtSerialize(4)])]);
-                $chest = ItemFactory::get(Block::CHEST, 10, 1);
+                $chest = VanillaBlocks::CHEST()->asItem(); // Use VanillaBlocks::CHEST()->asItem() to create a chest item
                 $chest->setNamedTagEntry($nbt);
                 $chest->setCustomName("Kit");
                 $sender->getInventory()->addItem($chest);
